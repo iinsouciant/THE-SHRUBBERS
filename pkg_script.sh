77d0c668a61fc15a04573f44adf0b1066f9f1416
Ryan Sands <sandsryanj@gmail.com>
2022-03-21 15:18:00 -0700

    test numpy

--- pkg_script.sh
+++ pkg_script.sh
@@ -6,11 +6,11 @@ PIP3_PKGS="Adafruit-Blinka adafruit-circuitpython-ads1x15 scipy"
 
 apt-get update
 apt-get upgrade -y
+apt remove python3-numpy -y
+pip install numpy
 apt-get install $APT_PKGS -y
 pip3 install $PIP3_PKGS 
-
-apt remove python3-numpy
-pip install numpy
+apt autoremove
 
 pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
