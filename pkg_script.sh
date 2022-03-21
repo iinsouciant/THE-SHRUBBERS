2933bea5333b415ce68326d13780f2daad6d971c
Ryan Sands <sandsryanj@gmail.com>
2022-03-21 14:44:29 -0700

    PKGS fix

--- pkg_script.sh
+++ pkg_script.sh
@@ -7,15 +7,15 @@ PIP3_PKGS="Adafruit-Blinka adafruit-circuitpython-ads1x15 scipy"
 apt-get update
 apt-get upgrade -y
 apt-get install $APT_PKGS -y
-pip3 install $PIP3_PKGS -y
+pip3 install $PIP3_PKGS 
 
-pip install numpy -y
+pip install numpy
 
 # enable i2c 
 CONFIG="/boot/config.txt"
 if grep -Fq "dtparam=i2c_arm" $CONFIG
 then
-    sed -i "s/dtparam=i2c_arm/c\dtparam=i2c_arm=on"
+    sed -i "/dtparam=i2c_arm/c\dtparam=i2c_arm=on"
 else
     echo "dtparam=i2c_arm=on" >> $CONFIG
 fi
