1670a05542380021af808d409069441650dee48e
Ryan Sands <sandsryanj@gmail.com>
2022-03-21 16:04:19 -0700

    ensure vnc viewer resolution is acceptable

--- pkg_script.sh
+++ pkg_script.sh
@@ -12,7 +12,36 @@ apt-get install $APT_PKGS -y
 pip3 install $PIP3_PKGS 
 apt autoremove
 
+# ensure 1024x768 res
+CONFIG="/boot/config.txt"
+if grep -Fq "hdmi_force_hotplug" $CONFIG
+then
+    sed -i "hdmi_force_hotplug/c\hdmi_force_hotplug=1"
+else
+    echo "hdmi_force_hotplug=1" >> $CONFIG
+fi
+
+if grep -Fq "hdmi_ignore_edid" $CONFIG
+then
+    sed -i "hdmi_ignore_edid/c\hdmi_ignore_edid=0xa5000080"
+else
+    echo "hdmi_ignore_edid=0xa5000080" >> $CONFIG
+fi
+
+if grep -Fq "hdmi_group" $CONFIG
+then
+    sed -i "hdmi_group/c\hdmi_group=2"
+else
+    echo "hdmi_group=2" >> $CONFIG
+fi
+
+if grep -Fq "hdmi_mode" $CONFIG
+then
+    sed -i "hdmi_mode/c\hdmi_mode=16"
+else
+    echo "hdmi_mode=16" >> $CONFIG
+fi
+
 pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 python3 raspi-blinka.py
-reboot
