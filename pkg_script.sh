3f3c68fa5d8d46366eef5114aaa7fd21ebb1f4ed
Ryan Sands <sandsryanj@gmail.com>
2022-03-21 17:14:10 -0700

    working? version to get numpy to install properly

--- pkg_script.sh
+++ pkg_script.sh
@@ -2,42 +2,44 @@
 
 # install the packages
 APT_PKGS="python3-gpiozero"
-PIP3_PKGS="Adafruit-Blinka adafruit-circuitpython-ads1x15 scipy"
+PIP3_PKGS="adafruit-circuitpython-ads1x15 scipy libcblas-dev"
 
 apt-get update
 apt-get upgrade -y
 apt remove python3-numpy -y
-pip install numpy
 apt-get install $APT_PKGS -y
 pip3 install $PIP3_PKGS 
+pip install numpy==1.20
+apt-get install libatlas-base-dev -y
+pip3 install --upgrade numpy
 apt autoremove
 
-# ensure 1024x768 res
+# ensure 1024x768 res for vnc
 CONFIG="/boot/config.txt"
 if grep -Fq "hdmi_force_hotplug" $CONFIG
 then
-    sed -i "hdmi_force_hotplug/c\hdmi_force_hotplug=1" $CONFIG
+    sed -i "/hdmi_force_hotplug/c\hdmi_force_hotplug=1" $CONFIG
 else
     echo "hdmi_force_hotplug=1" >> $CONFIG
 fi
 
 if grep -Fq "hdmi_ignore_edid" $CONFIG
 then
-    sed -i "hdmi_ignore_edid/c\hdmi_ignore_edid=0xa5000080" $CONFIG
+    sed -i "/hdmi_ignore_edid/c\hdmi_ignore_edid=0xa5000080" $CONFIG
 else
     echo "hdmi_ignore_edid=0xa5000080" >> $CONFIG
 fi
 
 if grep -Fq "hdmi_group" $CONFIG
 then
-    sed -i "hdmi_group/c\hdmi_group=2" $CONFIG
+    sed -i "/hdmi_group/c\hdmi_group=2" $CONFIG
 else
     echo "hdmi_group=2" >> $CONFIG
 fi
 
 if grep -Fq "hdmi_mode" $CONFIG
 then
-    sed -i "hdmi_mode/c\hdmi_mode=16" $CONFIG
+    sed -i "/hdmi_mode/c\hdmi_mode=16" $CONFIG
 else
     echo "hdmi_mode=16" >> $CONFIG
 fi
