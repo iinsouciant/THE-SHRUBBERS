6c4dbcdd17ccd1ddb1d57849625e09d6db8024a8
Ryan Sands <sandsryanj@gmail.com>
2022-04-11 14:49:21 -0700

    1 wire enable

--- pkg_script.sh
+++ pkg_script.sh
@@ -6,12 +6,12 @@ PIP3_PKGS="adafruit-circuitpython-ads1x15 scipy libcblas-dev"
 
 apt-get update
 apt-get upgrade -y
-#apt remove python3-numpy -y
+apt remove python3-numpy -y
 apt-get install $APT_PKGS -y
 pip3 install $PIP3_PKGS 
-#pip install numpy==1.20
-#apt-get install libatlas-base-dev -y
-#pip3 install --upgrade numpy
+pip install numpy==1.20
+apt-get install libatlas-base-dev -y
+pip3 install --upgrade numpy
 apt autoremove
 
 # ensure 1024x768 res for vnc
@@ -43,7 +43,7 @@ then
 else
     echo "hdmi_mode=16" >> $CONFIG
 fi
-
+# enable 1 wire interface for temp sensor
 if grep -Fq "dtoverlay" $CONFIG
 then
     sed -i "/dtoverlay/c\dtoverlay=w1-gpio" $CONFIG
