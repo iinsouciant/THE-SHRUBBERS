1f52c91fa490c748134c9ced44e44f80fc588d4a
Ryan Sands <sandsryanj@gmail.com>
2022-05-10 12:41:25 -0700

    test shell sciprt change

--- pkg_script.sh
+++ pkg_script.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # install the packages
-APT_PKGS="python3-gpiozero"
-PIP3_PKGS="adafruit-circuitpython-ads1x15 scipy libcblas-dev"
+APT_PKGS="python3-gpiozero libatlas-base-dev"
+PIP3_PKGS="adafruit-circuitpython-ads1x15 scipy GitPython libcblas-dev"
 
 apt-get update
 apt-get upgrade -y
 apt remove python3-numpy -y
-apt-get install $APT_PKGS -y
 pip3 install $PIP3_PKGS 
 pip install numpy==1.20
-pip install GitPython
-apt-get install libatlas-base-dev -y
 pip3 install --upgrade numpy
+apt-get install $APT_PKGS -y
 apt autoremove
 
 pip3 install --upgrade adafruit-python-shell
