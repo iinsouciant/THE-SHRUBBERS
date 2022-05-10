0b6f2c64e20345fab7dfe9fc310956b801e930cd
Ryan Sands <sandsryanj@gmail.com>
2022-05-10 13:31:24 -0700

    attempt to fix code not upating on boot by adding delay

--- autoupdate.py
+++ autoupdate.py
@@ -9,22 +9,23 @@ file = Path('shrubber_main.py')
 myRepoDir = file.parent.absolute()
 timeout = 10
 
-try:
-    # requesting URL
-    request = requests.get(url, timeout=timeout)
-    print("Internet is on")
+for _ in range(3):
+    try:
+        # requesting URL
+        request = requests.get(url, timeout=timeout)
+        print("Internet is on")
 
-    resetter = Repo(myRepoDir)
-    myRepo = Git(myRepoDir)
-    # revert local unsaved changes
-    resetter.git.reset('--hard')
-    # get latest commit
-    myRepo.fetch()
-    myRepo.pull('origin', 'main')
-    print('pulled latest program version')
+        resetter = Repo(myRepoDir)
+        myRepo = Git(myRepoDir)
+        # revert local unsaved changes
+        resetter.git.reset('--hard')
+        # get latest commit
+        myRepo.fetch()
+        myRepo.pull('origin', 'main')
+        print('pulled latest program version')
     
-except (requests.ConnectionError, requests.Timeout) as exception:
-    print("Internet is off")
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        print("Internet is off")
 
 
 system('python3 shrubber_main.py')
